f8c3176daece950051cfb6ccf0f9f337862e7df4
luoji <gunsluo@gmail.com>
2018-06-08 23:50:05 +0800

    update shell
    Signed-off-by: luoji <gunsluo@gmail.com>

--- version/download_k8s_cn.sh
+++ version/download_k8s_cn.sh
@@ -1,35 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
 ARCH=amd64
 mversion=v1.10.4
 nversion=1.14.8
 username=luoji
 password=password
+repo=k8s.gcr.io
 store_repo=r.xiaozhou.net
 store_repo_path=r.xiaozhou.net/kubernetes
- 
-images=(kube-apiserver-${ARCH}:${mversion} \
-	kube-controller-manager-${ARCH}:${mversion} \
-	kube-scheduler-${ARCH}:${mversion} \
-	kube-proxy-${ARCH}:${mversion} \
-	etcd-${ARCH}:3.1.12 \
-	pause-${ARCH}:3.1 \
-	k8s-dns-sidecar-${ARCH}:${nversion} \
-	k8s-dns-kube-dns-${ARCH}:${nversion} \
-	k8s-dns-dnsmasq-nanny-${ARCH}:${nversion} \
+
+images=(${repo}/kube-apiserver-${ARCH}:${mversion} \
+	${repo}/a/b/kube-controller-manager-${ARCH}:${mversion} \
+	${repo}/kube-scheduler-${ARCH}:${mversion} \
+	${repo}/kube-proxy-${ARCH}:${mversion} \
+	${repo}/etcd-${ARCH}:3.1.12 \
+	${repo}/pause-${ARCH}:3.1 \
+	${repo}/k8s-dns-sidecar-${ARCH}:${nversion} \
+	${repo}/k8s-dns-kube-dns-${ARCH}:${nversion} \
+	${repo}/k8s-dns-dnsmasq-nanny-${ARCH}:${nversion} \
+    quay.io/coreos/flannel:v0.10.0-amd64 \
 	)
 
 docker login -u $username -p $password $store_repo
- 
-for image in ${images[@]}
+
+for url in ${images[@]}
 do
-	docker pull ${store_repo_path}/${image}
-	docker tag ${store_repo_path}/${image} gcr.io/google_containers/${image}
-	docker rmi ${store_repo_path}/${image}
+    sub=${url%/*}
+    idx=${#sub}
+    image=${url:$idx+1}
+    echo -e "download -> $store_repo_path/$image -> $url"
+	docker pull $store_repo_path/$image
+	docker tag $store_repo_path/$image $url
+	docker rmi $store_repo_path/$image
 done
 
-docker pull ${store_repo_path}/flannel:v0.10.0-amd64
-docker tag ${store_repo_path}/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
-docker rmi ${store_repo_path}/flannel:v0.10.0-amd64
- 
-unset ARCH mversion nversion images username password store_repo store_repo_path
+unset ARCH mversion nversion images username password repo store_repo store_repo_path
--- version/upload_k8s_cn.sh
+++ version/upload_k8s_cn.sh
@@ -1,40 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 ARCH=amd64
 mversion=v1.10.4
 nversion=1.14.8
 username=luoji
 password=password
+repo=k8s.gcr.io
 store_repo=r.xiaozhou.net
 store_repo_path=r.xiaozhou.net/kubernetes
  
-images=(kube-apiserver-${ARCH}:${mversion} \
-	kube-controller-manager-${ARCH}:${mversion} \
-	kube-scheduler-${ARCH}:${mversion} \
-	kube-proxy-${ARCH}:${mversion} \
-	etcd-${ARCH}:3.1.12 \
-	pause-${ARCH}:3.1 \
-	k8s-dns-sidecar-${ARCH}:${nversion} \
-	k8s-dns-kube-dns-${ARCH}:${nversion} \
-	k8s-dns-dnsmasq-nanny-${ARCH}:${nversion} \
+
+images=(${repo}/kube-apiserver-${ARCH}:${mversion} \
+	${repo}/a/b/kube-controller-manager-${ARCH}:${mversion} \
+	${repo}/kube-scheduler-${ARCH}:${mversion} \
+	${repo}/kube-proxy-${ARCH}:${mversion} \
+	${repo}/etcd-${ARCH}:3.1.12 \
+	${repo}/pause-${ARCH}:3.1 \
+	${repo}/k8s-dns-sidecar-${ARCH}:${nversion} \
+	${repo}/k8s-dns-kube-dns-${ARCH}:${nversion} \
+	${repo}/k8s-dns-dnsmasq-nanny-${ARCH}:${nversion} \
+    quay.io/coreos/flannel:v0.10.0-amd64 \
 	)
- 
+
 docker login -u $username -p $password $store_repo
- 
-for image in ${images[@]}
-do
-    echo download/upload k8s.gcr.io/${image}
-	docker pull k8s.gcr.io/${image}
-	docker tag k8s.gcr.io/${image} ${store_repo_path}/${image}
-	docker push ${store_repo_path}/${image}
-	docker rmi k8s.gcr.io/${image}
-	docker rmi ${store_repo_path}/${image}
+
+for url in ${images[@]}
+do 
+    sub=${url%/*}
+    idx=${#sub}
+    image=${url:$idx+1}
+    echo -e "upload $url -> $store_repo_path/$image"
+	docker pull $url
+	docker tag $url $store_repo_path/$image
+	docker push $store_repo_path/$image
+	docker rmi $url
+	docker rmi $store_repo_path/$image
 done
 
-docker pull quay.io/coreos/flannel:v0.10.0-amd64
-docker tag quay.io/coreos/flannel:v0.10.0-amd64 ${store_repo_path}/flannel:v0.10.0-amd64
-docker push ${store_repo_path}/flannel:v0.10.0-amd64
-docker rmi quay.io/coreos/flannel:v0.10.0-amd64
-docker rmi ${store_repo_path}/flannel:v0.10.0-amd64
- 
-unset ARCH mversion nversion images username password store_repo store_repo_path
+unset ARCH mversion nversion images username password repo store_repo store_repo_path
