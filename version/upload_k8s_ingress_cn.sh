a5d7df9a89e63c7368d6fbc94aeeb61e8c8e040c
luoji <gunsluo@gmail.com>
2018-07-31 10:20:49 +0800

    ingress
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ version/upload_k8s_ingress_cn.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+username=luoji
+password=YYluoji123
+store_repo=r.xiaozhou.net
+store_repo_path=r.xiaozhou.net/kubernetes
+
+images=(gcr.io/google_containers/defaultbackend:1.4 \
+	)
+
+#docker login -u $username -p $password $store_repo
+
+for url in ${images[@]}
+do 
+    sub=${url%/*}
+    idx=${#sub}
+    image=${url:$idx+1}
+    echo -e "upload $url -> $store_repo_path/$image"
+	docker pull $url
+	docker tag $url $store_repo_path/$image
+	docker push $store_repo_path/$image
+	docker rmi $url
+	docker rmi $store_repo_path/$image
+done
+
+unset ARCH mversion nversion images username password store_repo store_repo_path
